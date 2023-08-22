715e2c2b31b8ba0b80f4c43fa7a4dccc89b0089f
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-22 12:54:01 +0200

    remove unnecessary @JsonProperty annotations

--- src/main/groovy/groovy/blog/TodoStats.groovy
+++ src/main/groovy/groovy/blog/TodoStats.groovy
@@ -1,12 +1,9 @@
 package groovy.blog
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.serde.annotation.Serdeable
 
 @Serdeable
-@JsonCreator
-record TodoStats(@JsonProperty('total') int total,
-                 @JsonProperty('completed') int completed,
-                 @JsonProperty('totalScheduled') int totalScheduled,
-                 @JsonProperty('completedOnSchedule') int completedOnSchedule) {}
+record TodoStats(int total,
+                 int completed,
+                 int totalScheduled,
+                 int completedOnSchedule) {}
